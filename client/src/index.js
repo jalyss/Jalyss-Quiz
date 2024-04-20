fb38634ba78c7875bd1ed0312ba5ecc0d3508a68
khalil kraiem <khalilmedkraiem@gmail>
2024-04-20 11:29:06 +0100

    remove stricMode index client

--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,10 @@ import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
     <ToastContainer />
       <Router />
     </Provider>
-  </React.StrictMode>
+  //</React.StrictMode> 
 );
