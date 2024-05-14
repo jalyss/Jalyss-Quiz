2fde3e6842d15e30ba49c6396442350848e6042f
mestiri77 <faroukmestiri77@gamil.com>
2024-05-14 18:53:03 +0100

    dev-backend : upload file done

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {cors:true});
+  app.useStaticAssets('upload', {prefix: '/upload'});
  
   const config = new DocumentBuilder()
   .setTitle('Jalyss-Quiz')
